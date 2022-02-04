74a0a019157248c1679e66b7a2cd1219179d8bd7
tinpham1510 <tinpham15102000@gmail.com>
2022-02-04 22:38:43 +0700

    third commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main()
+{
+    char str = "Hello World";
+    char str2 = "Hello World";
+    char str3 = "Hello World";
+    char str4 = "Hello World";
+    
+    printf("%s\n", str);
+    printf("%s\n", str2);
+    printf("%s\n", str3);
+    printf("%s\n", str4);
+    
+    return 0;
+}
